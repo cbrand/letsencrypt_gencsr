a95faba0ac987be1af5742f01ba1c3bb4bb6e71a
Christoph Brand <christoph@brand.rest>
2015-12-05 18:09:24 +0100

    cmd: cloned functionality of the original command client. Injected gencsr into the entry.

--- src/letsencrypt_gencsr/cmd/gencsr.py
+++ src/letsencrypt_gencsr/cmd/gencsr.py
@@ -1,19 +1,16 @@
 # -*- encoding: utf-8 -*-
 
 from __future__ import unicode_literals, absolute_import, print_function
-
 import os
-
 from letsencrypt.crypto_util import init_save_csr
 from letsencrypt.le_util import Key
 
 
-def gencsr(args):
-    if not os.path.isfile(args.key):
+def gencsr(args, config, plugins):
+    if not args.private_key:
         print('The given key argument does not point to a file.')
 
-    with open(args.key, 'r') as f:
-        key = Key(args.key, f.read())
+    key = args.private_key[1]
 
     init_save_csr(
         key,
@@ -21,4 +18,3 @@ def gencsr(args):
         os.path.dirname(args.out) or '.',
         os.path.basename(args.out)
     )
-
